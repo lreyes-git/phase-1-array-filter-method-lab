a22614372a757486eff7cbd8342694604b7aa7b1
Leonardo Reyes <reyesleonardo44@yahoo.com>
2023-05-11 17:05:21 -0700

    Array filter method lab complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+const findMatching = function(collection, str) {
+    const matchingDrivers = [];
+    for (const user of collection) {
+        if (user.toLowerCase() === str.toLowerCase())
+        matchingDrivers.push(user);
+    }
+    return matchingDrivers;
+}
+
+const fuzzyMatch = function(collection, str) {
+    const fuzzyList = [];
+    for (const user of collection) {
+        if (user.charAt(0) === str.charAt(0))
+        fuzzyList.push(user);
+    }
+    return fuzzyList;
+}
+
+const matchName = function(collection, str) {
+    const newNames = [];
+    for (const user of collection) {
+        if (user.name === str)
+        newNames.push(user);
+    }
+    return newNames;
+}
